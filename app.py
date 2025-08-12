b5204e25368e91652ee2b995025d926ff6c6791c
cactuskat <seriousbusiness401@gmail.com>
2025-08-11 22:14:44 -0400

    Implemented Weather Feature
    .Completed Task 1(implement weather search) of Sprint 2 .Completed Task 2(weather search depending on zipcode or default console location)

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-import sqlite3
-from flask import Flask, jsonify, render_template, request
+import sqlite3, os, requests
+from flask import Flask, jsonify, render_template, request, request
+from dotenv import load_dotenv
 
+load_dotenv()
 app = Flask(__name__)
 
 @app.route("/")
@@ -11,6 +13,8 @@ def home():
 def search_clothing():
     type_param = request.args.get("type")
     weather_param = request.args.get("weather")
+    if not type_param or not weather_param:
+        return jsonify({"error": "Missing type_param or weather_param"})
     print(f"Recieved type: {type_param}, weather:{weather_param}")
     #print("hello world")
     query = "SELECT id,name,img_path FROM closet WHERE type = ? AND weather = ? ORDER BY RANDOM() LIMIT 1"
@@ -31,6 +35,38 @@ def search_clothing():
     except Exception as e:
         return jsonify({"error": "error searching closet_list.db"})
     
+
+@app.route("/weather/get/today")
+def get_temperature():
+    #weather_api_key = os.getenv("OPENWEATHER_API_KEY")
+    #demo key from publicly provided by geeksforgeeks is used so
+    #i can publicly share this project without making other require .env
+    weather_api_key = "6d055e39ee237af35ca066f35474e9df"
+    lat_coord = request.args.get("lat")
+    lon_coord = request.args.get("lon")
+    zipcode = request.args.get("zipcode")
+
+    if lat_coord and lon_coord:
+        url = (f"https://api.openweathermap.org/data/2.5/weather?"
+            f"lat={lat_coord}&lon={lon_coord}&appid={weather_api_key}&units=imperial")
+        print(f"Recieved lat: {lat_coord}, lon: {lon_coord}")
+    elif zipcode:
+        url = (f"https://api.openweathermap.org/data/2.5/weather?"
+            f"zip=29607&appid={weather_api_key}&units=imperial")
+        print(f"Recieved zipcde:{zipcode}")
+    else:
+        return jsonify({"error": "Must provide lat/lon or zipcode"})
+
+    try:
+        response  = requests.get(url,timeout = 5)
+        if response.status_code == 200:
+            result = response.json()
+            return jsonify({"temp": result["main"]["feels_like"]})
+        else: 
+            return jsonify({"error": f"Openweather returned: {response.status_code}"})
+    except Exception as e:
+        return jsonify({"error": "error fetching data from OpenWeather API"})
+
 
 
 if __name__ == '__main__':
